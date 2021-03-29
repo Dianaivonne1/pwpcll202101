1fa031795b7d56779573d7762422f9a4baeef47b
Dianaivonne <L171130052@gamadero.tecnm.mx>
2021-03-29 15:31:08 -0600

    Proyecto es estado inicial funcionando

---
+++ routes/index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express',author: 'Diana Ivonne', appName: 'WebApp' });
+});
+
+
+/* Agregando nueva ruta */
+router.get('/greeting', function(req, res, next) {
+  res.status(200).json({message: 'Hola campeon'})
+})
+
+router.get('/diana', function(req, res, next) {
+  res.send('Hola Diana')
+})
+
+module.exports = router;
