23d7864017fe923925fcd76e93c1e02b6ee82e8e
Dianaivonne1 <L171130052@gamadero.tecnm.mx>
2021-05-25 21:26:43 -0500

    Sesion 26

--- client/index.js
+++ client/index.js
@@ -1,26 +1,28 @@
-import './stylesheets/style.css'
-import './stylesheets/mystyles.css'
+/* eslint-disable no-console */
+/* eslint-disable no-alert */
+import './stylesheets/style.css';
+import './stylesheets/mystyles.css';
 
-console.log("Webpack Working!!!");
-//Default parameters ES6/2015
-let show = (m = "Hot Module Replacement Working") => {
-    alert(m)
+console.log('Webpack Working!!!');
+// Default parameters ES6/2015
+const show = (m = 'Hot Module Replacement Working') => {
+  alert(m);
 };
 
 show();
 
-function resolveAfter2seconds(){
-    return new Promise( resolve => {
-        setTimeout(()=>{
-            resolve('resolved')
-        }, 2000);
-    });
+function resolveAfter2seconds() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve('resolved');
+    }, 2000);
+  });
 }
 
-async function asyncCall(){
-    console.log("Calling an async function");
-    const result = await resolveAfter2seconds();
-    console.log(result);
+async function asyncCall() {
+  console.log('Calling an async function');
+  const result = await resolveAfter2seconds();
+  console.log(result);
 }
 
 asyncCall();
