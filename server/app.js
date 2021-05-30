e17aa0faeef801175f60810064f76f106eef0b48
Dianaivonne1 <L171130052@gamadero.tecnm.mx>
2021-05-30 18:23:52 -0500

    Sesion 27

--- server/app.js
+++ server/app.js
@@ -1,70 +1,75 @@
-import createError from 'http-errors';
-import express from'express';
-import path from 'path';
-import cookieParser from 'cookie-parser';
-import logger from 'morgan';
+/* eslint-disable no-console */
+import createError from "http-errors";
+import express from "express";
+import path from "path";
+import cookieParser from "cookie-parser";
+import logger from "morgan";
 
 import indexRouter from '@s-routes/index';
 import usersRouter from '@s-routes/users';
 
-//Webpack Modules
-import webpack from 'webpack';
-import webpackDevMiddleware from 'webpack-dev-middleware';
-import webpackHotMiddleware from 'webpack-hot-middleware';
-import webpackConfig from '../webpack.dev.config'
-import webpackDevConfig from '../webpack.dev.config';
+// Webpack Modules
+import webpack from "webpack";
+import webpackDevMiddleware from "webpack-dev-middleware";
+import webpackHotMiddleware from "webpack-hot-middleware";
+import webpackDevConfig from "../webpack.dev.config";
 
-//Consultar el modo en que se esta ejecutando la aplicacion 
-const env = process.env.NODE_ENV || 'developement';
+// Consultar el modo en que se esta ejecutando la aplicacion 
+const env = process.env.NODE_ENV || "developement";
 
-//Se crea la aplicacion express
-var app = express();
+// Se crea la aplicacion express
+const app = express();
 
-//Verificando el modo de ejecucion de la aplicacion 
-if(env === 'development'){
-  console.log('> Excecuting in Development Mode: Webpack Hot Reloading');
-  //Paso 1. Agregando la ruta del HMR
-  //reload=true: Habilita la recarga del frontend cuando hay cambios en el codigo fuente del frontend
-  //timeout=1000: Tiempo de espera entre recarga  y recarga de la pagina 
-  webpackConfig.entry = ['webpack-hot-middleware/client?reload=true&timeout=1000',webpackConfig.entry];
+// Verificando el modo de ejecucion de la aplicacion 
+if (env === "development") {
+  console.log("> Excecuting in Development Mode: Webpack Hot Reloading");
+  // Paso 1. Agregando la ruta del HMR
+  // reload=true: Habilita la recarga del frontend cuando hay cambios en el codigo fuente del frontend
+  // timeout=1000: Tiempo de espera entre recarga  y recarga de la pagina 
+  webpackDevConfig.entry = [
+    "webpack-hot-middleware/client?reload=true&timeout=1000",
+    webpackDevConfig.entry,
+  ];
 
-  //Paso 2. Agregamos el Plugin
-  webpackConfig.plugins.push(new webpack.HotModuleReplacementPlugin());
+  // Paso 2. Agregamos el Plugin
+  webpackDevConfig.plugins.push(new webpack.HotModuleReplacementPlugin());
 
-  //Paso 3. Crear el compilador de webpack 
-  const compiler = webpack(webpackConfig);
+  // Paso 3. Crear el compilador de webpack 
+  const compiler = webpack(webpackDevConfig);
 
-  //Paso 4. Agregando el middleware a la cadena de middlewares de nuestra aplicacion
-  app.use(webpackDevMiddleware(compiler,{
-    publicPath: webpackDevConfig.output.publicPath
-  }));
+  // Paso 4. Agregando el middleware a la cadena de middlewares de nuestra aplicacion
+  app.use(
+    webpackDevMiddleware(compiler, {
+      publicPath: webpackDevConfig.output.publicPath,
+    })
+  );
 
-  //Paso 5. Agregando el Webpack Hot Middleware
+  // Paso 5. Agregando el Webpack Hot Middleware
   app.use(webpackHotMiddleware(compiler));
-}else{
-  console.log('> Excecuting in Production Mode...');
+} else {
+  console.log("> Excecuting in Production Mode...");
 }
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname,'..', 'public')));
+app.use(express.static(path.join(__dirname, "..", "public")));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
