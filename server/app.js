d72f4549293717ac1c3b4bf3523e1aa7ccc1c0ef
Dianaivonne1 <L171130052@gamadero.tecnm.mx>
2021-06-04 18:37:33 -0500

    Sesion 29

--- server/app.js
+++ server/app.js
@@ -3,43 +3,38 @@ import createError from 'http-errors';
 import express from 'express';
 import path from 'path';
 import cookieParser from 'cookie-parser';
-import logger from "morgan";
-
+import morgan from 'morgan';
+import winston from 'winston';
 import indexRouter from '@s-routes/index';
 import usersRouter from '@s-routes/users';
-
 // Importing configurations
-import configTemplateEngine from '@s-config/template-engine';
-
+import configTemplateEngine from '@s-config/template-engine'
 // Webpack Modules
 import webpack from 'webpack';
 import webpackDevMiddleware from 'webpack-dev-middleware';
 import webpackHotMiddleware from 'webpack-hot-middleware';
 import webpackDevConfig from '../webpack.dev.config';
-
 // Consultar el modo en que se esta ejecutando la aplicacion 
 const env = process.env.NODE_ENV || 'developement';
-
 // Se crea la aplicacion express
 const app = express();
-
 // Verificando el modo de ejecucion de la aplicacion 
 if (env === 'development'){
   console.log('> Excecuting in Development Mode: Webpack Hot Reloading');
   // Paso 1. Agregando la ruta del HMR
   // reload=true: Habilita la recarga del frontend cuando hay cambios en el codigo fuente del frontend
   // timeout=1000: Tiempo de espera entre recarga  y recarga de la pagina 
+
   webpackDevConfig.entry = [
     'webpack-hot-middleware/client?reload=true&timeout=1000',
     webpackDevConfig.entry,
   ];
-
   // Paso 2. Agregamos el Plugin
   webpackDevConfig.plugins.push(new webpack.HotModuleReplacementPlugin());
 
   // Paso 3. Crear el compilador de webpack 
   const compiler = webpack(webpackDevConfig);
-
+  
   // Paso 4. Agregando el middleware a la cadena de middlewares de nuestra aplicacion
   app.use(
     webpackDevMiddleware(compiler, {
@@ -50,17 +45,17 @@ if (env === 'development'){
   // Paso 5. Agregando el Webpack Hot Middleware
   app.use(webpackHotMiddleware(compiler));
 } else {
-  console.log("> Excecuting in Production Mode...");
+  console.log('> Excecuting in Production Mode...');
 }
 
 // view engine setup
 configTemplateEngine(app);
 
-app.use(logger("dev"));
+app.use(morgan('combined', { stream: winston.stream }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "..", "public")));
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
