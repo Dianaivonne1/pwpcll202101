0fda3f7a4988e52699e11049f002cc32bbb927a3
Dianaivonne1 <L171130052@gamadero.tecnm.mx>
2021-06-28 22:47:30 -0500

    Sesion 32

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,7 +1,7 @@
 const index = (req, res) => {
-    res.send('Indexado de usuarios ... Under Construction.');
+  res.send('Indexado de usuarios ... Under Construction.');
 };
 
 export default {
-    index,
+  index,
 }
